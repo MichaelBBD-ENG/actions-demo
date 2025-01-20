1d2bcc9efe9960e47f27a83916e4308d3358e1a3
MichaelBBD-ENG <michael.chinyama@bbd.co.za>
2025-01-20 18:00:09 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 export const handler = async (event) => {
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! This function was deployed from zip by Michael'),
+        body: JSON.stringify('Hello from Lambda!  This function was deployed from zip by Michael'),
     };
 };
