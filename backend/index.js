aec6cdc864a98994f745f7051fb7ab3919b45400
Michael-u21546551 <michael.chinyama@bbd.co.za>
2025-01-20 15:36:52 +0200

    Adding aws lambda code

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-// Define the single endpoint
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello, this is a simple API response!' });
-});
-
-// Start the server
-app.listen(port, () => {
-    console.log(`API is running at http://localhost:${port}`);
-});
+export const handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda! This function was deployed from zip by Michael'),
+    };
+};
